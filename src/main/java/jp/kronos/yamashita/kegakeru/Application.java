c466edab83e02c88528c3bbc0859680c6c2a8773
mizuki_yamashita <mizuki_yamashita@kronos.jp>
2018-02-09 17:10:34 +0900

    初期画面表示確認

--- src/main/java/jp/kronos/yamashita/kegakeru/Application.java
+++ src/main/java/jp/kronos/yamashita/kegakeru/Application.java
@@ -2,18 +2,14 @@ package jp.kronos.yamashita.kegakeru;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 
 @SpringBootApplication
 @RestController
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @RequestMapping(value = "/")
-    String hello() {
-        return "Hello World!";
     }
 }
